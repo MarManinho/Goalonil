e62eb2efa7c59bdb81ed66e13eb94b23e68a7882
MarManinho <mohammad.gharib1378@gmail.com>
2025-05-10 15:03:49 +0200

    Initial project setup

---
+++ Goalonil/Goalonil/GoalonilApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalonilApp.swift
+//  Goalonil
+//
+//  Created by Mohammad Gharib Joorkouyeh on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GoalonilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
